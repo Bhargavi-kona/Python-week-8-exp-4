8d356fdae759794e6f8dbafefc36798ab6655809
Bhargavi-kona <24331a05e1@mvgrce.edu.in>
2026-02-22 23:03:55 +0530

    Add files via upload

---
+++ w8.4.py
@@ -0,0 +1,5 @@
+dict1 = {11:"conrad", 21:"belly", 31:"jere", 41:"taylor", 51:"steven"}
+dict1.update({61:"susannah"})
+print("After adding key value pair:", dict1)
+dict1.update({21:"isabel"})
+print("After updating key 21:", dict1)
